6f7e0a0a8a8da736dae3fff66971fbe63f860bd4
Tosiapenas <galdiniguilherme@usp.br>
2022-09-04 13:35:58 -0300

    Heapsort algorithm: implementing and getting the indexes that had been changed

--- C1 - Bubble Insertion Selection/bubble_sort.c
+++ C1 - Bubble Insertion Selection/bubble_sort.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 100000
+#define MAX 1000
 
-void swap(int *x, int *y);
+//BubbleSort header
 void BubbleSort(int *arr, int ARRAY_SIZE);
+
+//Auxiliar functions headers
+void swap(int *x, int *y);
 void print_array(int *arr, int ARRAY_SIZE);
 
 int main(){
 
-  int *arr = malloc(MAX*sizeof(int));
   clock_t begin = clock();
   srand(time(NULL));
+  int *arr = malloc(MAX*sizeof(int));
 
   for(int i = 0; i < MAX; i++){
     arr[i] = (rand() % 1000);
@@ -19,9 +22,10 @@ int main(){
 
   BubbleSort(arr, MAX);
 
+
   clock_t end = clock();
   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-
+  
   printf("\n\n%.2lf\n", time_spent);
 
   free(arr);
@@ -37,7 +41,7 @@ void swap(int *x, int *y){
 void print_array(int *arr, int ARRAY_SIZE){
 
     for(int i = 0; i < ARRAY_SIZE; i++) {
-      printf ("%d ", *(arr +i));
+      printf ("%d ", *(arr + i));
   }
   printf ("\n");
 }
@@ -51,7 +55,6 @@ void BubbleSort(int *arr, int ARRAY_SIZE) {
       array_end = previous_modification;
         for(int j = 0; j < array_end - 1; j++){
             //printf("C %d %d\n", j, j + 1);
-
             if(arr[j] > arr[j + 1]){
                 previous_modification = j + 1;
                 //printf("T %d %d\n", j, previous_modification);
