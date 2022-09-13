7e7b0b321a7c189ac1e55798c355a7f5fa58bdb6
Tosiapenas <galdiniguilherme@usp.br>
2022-09-13 06:34:34 -0300

    Added function for reading arrays (in exercise 2)

--- Exercises - Modular/Exercise 2/sort.c
+++ Exercises - Modular/Exercise 2/sort.c
@@ -1,62 +1,11 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define SERVICE_UNAVAILABLE 500
-
-void merge_logic(int *array, int array1_start, int mid, int array2_start, int array2_end, int *aux_vet, int *comparisons, int *changes);
-void merge(int *array, int array_start, int array_end, int *aux_vet, int *comparisons, int *changes);
-void mergesort(int *array, int array_size);
-void insertion_sort(int *arr, int array_size);
-void free_matrix(int **matrix, int rows);
-int scanf_array(int *arr, int ARRAY_SIZE);
-
-int main(){
-
-    int Q, *N, **arrays_matrix; // respectivly: arrays size, arrays size and matrix of arrays
-    int *aux_array; //for mergesort
-
-    scanf("%d", &Q);
-    N = (int *) malloc(Q*sizeof(int));
-    arrays_matrix = (int**) malloc(Q*sizeof(int*));
-
-    if(N == NULL || arrays_matrix == NULL){
-        return SERVICE_UNAVAILABLE;
-    }
-
-    scanf_array(N, Q); //scanf &N[i] Q times
-
-    for(int i = 0; i < Q; i++){
-        arrays_matrix[i] = (int*)malloc(N[i]*sizeof(int));
-        if(arrays_matrix[i] == NULL){
-            return SERVICE_UNAVAILABLE;
-        }
-    }
-
-    //### STARTING LOOP OF SORTING LOGIC ### //
-    for(int i = 0; i < Q; i++){
-        aux_array = (int *) malloc(N[i]*sizeof(int)); // aux array -> necessary for mergesort
-        for(int j = 0; j < N[i]; j++){
-            scanf("%d", &arrays_matrix[i][j]);
-            aux_array[j] = arrays_matrix[i][j];
-        }
-        insertion_sort(arrays_matrix[i], N[i]);
-        mergesort(aux_array, N[i]);
-        free(aux_array); // necessary to do here, else the arrays doesn't will be resetted (trust me)
-    }
-
-  free(N);
-  free_matrix(arrays_matrix, Q);
-  return 0;
-}
+#include "sort.h"
 
 void merge_logic(int *array, int array1_start, int mid, int array2_start, int array2_end, int *aux_vet, int *comparisons, int *changes){
     int indexes_array1 = array1_start, indexes_array2 = array2_start, aux_array_indexes = 0;
 
-
-    if(array1_start >= array2_end){
-        exit(SERVICE_UNAVAILABLE);
-    }
-    
     while (indexes_array1 <= mid && indexes_array2 <= array2_end){
         (*comparisons)++;
         (*changes)++;
@@ -79,7 +28,7 @@ void merge_logic(int *array, int array1_start, int mid, int array2_start, int ar
     }
 
     aux_array_indexes = 0; //resetting variable at the final for join the splitted arrays
-    for(int i = array1_start; i <= array2_end; i++, aux_array_indexes++){
+    for (int i = array1_start; i <= array2_end; i++, aux_array_indexes++){
         (*changes)++; //if necessary while joining array1 in array2
         array[i] = aux_vet[aux_array_indexes];
     }
@@ -99,12 +48,11 @@ void merge(int *array, int array_start, int array_end, int *aux_vet, int *compar
 }
 
 void mergesort(int *array, int array_size){
-    
     int *aux_vet = (int *)malloc(array_size*sizeof(int)), change = 0, compare = 0;
 
     if(aux_vet == NULL){
         printf("Impossible to allocate memory...");
-        exit(SERVICE_UNAVAILABLE);
+        return;
     }
 
     merge(array, 0, array_size - 1, aux_vet, &compare, &change);
@@ -132,13 +80,6 @@ void insertion_sort(int *arr, int array_size){
     arr[j + 1] = aux;
   }
   printf("I %d %d %d\n", array_size, changes, comparisons);
-}
-
-int scanf_array(int *arr, int ARRAY_SIZE){
-    for(int i = 0; i < ARRAY_SIZE; i++){
-        scanf("%d", &(*(arr + i)));
-    }
-    return *arr;
 }
 
 void free_matrix(int **matrix, int rows){
