54f1adade578eb55f99a2942d00b7f524ba71ef4
Tosiapenas <galdiniguilherme@usp.br>
2022-09-21 20:48:50 -0300

    quicksort modification: using random index to be pivot

--- C5_6 - Quicksort/quicksort.c
+++ C5_6 - Quicksort/quicksort.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // function prototype
 void quick_sort(int *arr, int start, int end);
  
 //main
-int main()
-{
- int arr[7];
+int main(){
+
+ int size;
+ scanf("%d", &size);
+ int *arr = malloc(size*sizeof(int));
+
+ srand(time(NULL));
  
- // read 7 value from keyboard
- for(int i = 0; i < 7; i++){
+ for(int i = 0; i < size; i++){
   scanf("%d", &arr[i]);
  }
  
  //order values until (len(arr) - 1) --> 6
- quick_sort(arr, 0, 6);
+ quick_sort(arr, 0, size - 1);
  
  //print values in order ascendant
- for(int i = 0; i < 7; i++)
+ for(int i = 0; i < size; i++)
   printf("%d ", arr[i]);
  
  return 0;
@@ -30,7 +34,8 @@ void quick_sort(int *arr, int start, int end) {
      
     left = start;
     right = end;
-    mid = arr[(start + end) / 2];
+    int randomIndex = rand() / (RAND_MAX / end + 1);
+    mid = arr[randomIndex];
      
     while(left <= right) {
         while(arr[left] < mid && left < end) {
